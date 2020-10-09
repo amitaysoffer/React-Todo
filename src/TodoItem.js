42bb0b1953870098db98cf857ada217795008fb6
amitaysoffer <amitaysof@gmail.com>
2020-10-09 14:00:18 +0100

    todo app basic functions with bootstrap ready

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,29 +7,18 @@ function TodoItem(props) {
     color: '#cdcdcd',
     textDecoration: 'line-through'
   }
-  // const btnStyle = {
-  //   background: "#ff0000",
-  //   color: "#fff",
-  //   border: "none",
-  //   padding: "5px 9px",
-  //   borderRadius: "50%",
-  //   cursor: "pointer",
-  //   float: "right",
-  // };
 
   const { id, completed, text } = props.item;
 
   return (
     <li className="todo-item list-group-item-action list-group-item d-flex justify-content-between align-items-center">
-        <input className="" type="checkbox" checked={completed} onChange={() => props.toggleComplete(id)} />
-        <div className="col-10" style={completed ? styleCompleted : null}> {text} </div>
-        <button className=" btn btn-danger delete-button" onClick={() => props.deleteItem(id)}>X</button>
+      <input  type="checkbox" checked={completed} onChange={() => props.toggleComplete(id)} />
+      <div className="col-10" style={completed ? styleCompleted : null}> {text} </div>
+      <button className= "btn btn-danger delete-button" onClick={() => props.deleteItem(id)}>X</button>
 
-      </li>
-    
+    </li>
+
   )
 }
-
-
 
 export default TodoItem
